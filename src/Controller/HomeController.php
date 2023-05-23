440d2ab1bb67e97141e50ce2e001600e5fe24538
ETIENNE_JOUAULT <etienne.jouaulmt@live.fr>
2023-05-23 16:47:02 +0200

    tic & tac finish

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,18 @@ use App\Repository\SquirrelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\NutRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(SquirrelRepository $squirrelRepository): Response
+    public function index(SquirrelRepository $squirrelRepository, NutRepository $nutRepository ): Response
     {
         $squirrels = $squirrelRepository->findAll();
+        $nuts = $nutRepository->findAll();
         return $this->render('home/index.html.twig', [
             'squirrels' => $squirrels,
+            'nuts' => $nuts,
         ]);
     }
 }
